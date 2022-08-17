b449a37f21c351ccb31383d965d9c95258cf78a4
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-17 09:56:06 +0530

    feat: added source-map

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const webpackConfig = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
