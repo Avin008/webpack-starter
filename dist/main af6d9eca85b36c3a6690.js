755c7aab922180537778821058de467d2d0f9173
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-17 10:14:36 +0530

    feat: added babel

---
+++ dist/main af6d9eca85b36c3a6690.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.getElementById("root").innerHTML="".concat("<h1>VanillaJs Starter</h1>")}();
+//# sourceMappingURL=main af6d9eca85b36c3a6690.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,20 @@ const webpackConfig = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   devtool: "source-map",
   devServer: {
     static: {
