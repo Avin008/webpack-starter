378980f95a82b6ce5ec1ed362005e7e59e893066
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-17 09:26:21 +0530

    feat: added htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
+  mode: "production",
   entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "VanillaJs Starter",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
+
+module.exports = webpackConfig;
